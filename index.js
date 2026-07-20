e8f7e220e5683e6edd31f79fdefc7838e49576fc
neiranie <email_anda@contoh.com>
2026-07-20 15:12:07 +0700

    update index.js

--- index.js
+++ index.js
@@ -20,3 +20,23 @@ db.sequelize.sync()
     .catch((err) => {
         console.log(err);
     })
+
+
+    app.post("/komik", async (req, res) => {
+        const data = req.body;
+        try {
+            const komik = await db.Komik.create(data);
+            res.send(komik);
+        } catch (err) {
+            res.send(err);
+        }
+    });
+
+    app.get('/komik', async (req, res) => {
+        try {
+            const komik = await db.Komik.findAll();
+            res.send(komik);
+        } catch (err) {
+            res.send(err);
+        }
+    });
